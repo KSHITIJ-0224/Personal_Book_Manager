b8ace57651cad6e664947211cf9bb6b49b426481
KSHITIJ-0224 <chavankshitij170@gmail.com>
2026-03-03 15:52:20 +0530

    frontend url

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "*",
+    credentials: true
+}));
 app.use(morgan("dev"));
 app.use(express.json());
 
